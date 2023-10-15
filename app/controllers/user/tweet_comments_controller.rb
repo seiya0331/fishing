dfc83d40c8ee3c87eb19a1f3e15d4507f060759a
SeiyaYamaguchi <141209682+seiya0331@users.noreply.github.com>
2023-10-15 09:54:03 +0000

    コメント非同期化

--- app/controllers/user/tweet_comments_controller.rb
+++ app/controllers/user/tweet_comments_controller.rb
@@ -1,16 +1,14 @@
 class User::TweetCommentsController < ApplicationController
   def create
-    tweet = Tweet.find(params[:tweet_id])
-    comment = current_user.tweet_comments.new(tweet_comment_params)
-    comment.tweet_id = tweet.id
-    comment.save
-    redirect_to request.referer
+    @tweet = Tweet.find(params[:tweet_id])
+    @comment = current_user.tweet_comments.new(tweet_comment_params)
+    @comment.tweet_id = @tweet.id
+    @comment.save
   end
 
   def destroy
-    comment = TweetComment.find(params[:id])
-    comment.destroy
-    redirect_to request.referer
+    @comment = TweetComment.find(params[:id])
+    @comment.destroy
   end
 
   private
