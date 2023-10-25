eaed7352e1b7ca42728288e21e439145dff47f67
SeiyaYamaguchi <141209682+seiya0331@users.noreply.github.com>
2023-10-25 07:56:02 +0000

    seedファイル修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,20 +29,63 @@ end
   user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user3.jpg"), filename:"sample-user3.jpg")
 end
 
-Tweet.find_or_create_by! do |tweet|
-  tweet.get_image.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post1.jpg"), filename:"sample-post1.jpg")
-  tweet.text = "大人気のカフェです。"
+イカ = User.find_or_create_by!(email: "ika@sample.com") do |user|
+  user.name = "イカ"
+  user.password = "123456"
+  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user4.jpg"), filename:"sample-user4.jpg")
+end
+
+ブリ = User.find_or_create_by!(email: "buri@sample.com") do |user|
+  user.name = "ブリ"
+  user.password = "123456"
+  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user5.jpg"), filename:"sample-user5.jpg")
+end
+
+バス = User.find_or_create_by!(email: "basu@sample.com") do |user|
+  user.name = "バス"
+  user.password = "123456"
+  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user6.jpg"), filename:"sample-user6.jpg")
+end
+
+タコ = User.find_or_create_by!(email: "tako@sample.com") do |user|
+  user.name = "タコ"
+  user.password = "123456"
+  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user7.jpg"), filename:"sample-user7.jpg")
+end
+
+
+
+Tweet.find_or_create_by!(text: "新しいリールを書いました。") do |tweet|
+  tweet.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post1.jpg"), filename:"sample-post1.jpg")
   tweet.user = 魚
 end
 
-Tweet.find_or_create_by! do |tweet|
-  tweet.get_image.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post2.jpg"), filename:"sample-post2.jpg")
-  tweet.text = "日本料理は美しい！"
+Tweet.find_or_create_by!(text: "大きいマグロを釣るぞ！！") do |tweet|
+  tweet.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post2.jpg"), filename:"sample-post2.jpg")
   tweet.user = マグロ
 end
 
-Tweet.find_or_create_by! do |tweet|
-  tweet.get_image.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post3.jpg"), filename:"sample-post3.jpg")
-  tweet.text = 'メキシコ料理好きな方にオススメ！'
+Tweet.find_or_create_by!(text: "タイを釣りました。刺身にして食べます。") do |tweet|
+  tweet.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post3.jpg"), filename:"sample-post3.jpg")
   tweet.user = タイ
+end
+
+Tweet.find_or_create_by!(text: "イカを釣りに来ました。") do |tweet|
+  tweet.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post4.jpg"), filename:"sample-post4.jpg")
+  tweet.user = イカ
+end
+
+Tweet.find_or_create_by!(text: "でかいブリが釣りました。") do |tweet|
+  tweet.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post5.jpg"), filename:"sample-post5.jpg")
+  tweet.user = ブリ
+end
+
+Tweet.find_or_create_by!(text: "40センチ以上が釣れました。") do |tweet|
+  tweet.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post6.jpg"), filename:"sample-post6.jpg")
+  tweet.user = バス
+end
+
+Tweet.find_or_create_by!(text: "フィッシュキャッチャーがあると便利です。") do |tweet|
+  tweet.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post7.jpg"), filename:"sample-post7.jpg")
+  tweet.user = タコ
 end
